940776f57046249bf65bfab689a7123ab0a913b6
nxdir-s <snadir177@gmail.com>
2025-08-28 00:24:27 -0400

    update setup script

--- scripts/setup_k3spi.sh
+++ scripts/setup_k3spi.sh
@@ -2,26 +2,27 @@
 
 set -o errexit
 
-k taint nodes jetson gpu=true:NoSchedule
-k label node jetson node-role.kubernetes.io/worker=
-k label node jetson node-role.kubernetes.io/gpu=
+kubectl taint nodes jetson gpu=true:NoSchedule
+kubectl label node jetson node-role.kubernetes.io/worker=
+kubectl label node jetson node-role.kubernetes.io/gpu=
 
-k label nodes k3s1 k3s2 jetson node-role.kubernetes.io/kafka=
-k label nodes k3s1 k3s2 k3s3 node-role.kubernetes.io/postgres=
-k label nodes k3s1 k3s2 k3s3 node-role.kubernetes.io/cpu=
+kubectl label nodes k3s1 k3s2 jetson node-role.kubernetes.io/kafka=
+kubectl label nodes k3s1 k3s2 k3s3 node-role.kubernetes.io/postgres=
+kubectl label nodes k3s1 k3s2 k3s3 node-role.kubernetes.io/cpu=
 
-k apply -f secrets/cf-api-key.yaml
-k apply -f secrets/cf-email.yaml
+kubectl apply -f secrets/cf-api-key.yaml
+kubectl apply -f secrets/cf-email.yaml
 
-k create namespace cert-manager
-k create namespace longhorn-system
-k create namespace kafka
+kubectl create namespace cert-manager
+kubectl create namespace longhorn-system
+kubectl create namespace kafka
 
-k apply -f secrets/cf-api-key.yaml -n cert-manager
+kubectl apply -f secrets/cf-api-key.yaml -n cert-manager
 
-k create secret generic basic-auth --from-file=secrets/auth -n longhorn-system
+kubectl create secret generic basic-auth --from-file=secrets/auth -n longhorn-system
 
-k apply -f https://github.com/grafana/alloy-operator/releases/latest/download/collectors.grafana.com_alloy.yaml
+kubectl apply -f https://github.com/grafana/alloy-operator/releases/latest/download/collectors.grafana.com_alloy.yaml
+
 helm install grafana grafana/k8s-monitoring --namespace grafana --create-namespace --values secrets/grafana.yaml
 
 flux bootstrap github \
