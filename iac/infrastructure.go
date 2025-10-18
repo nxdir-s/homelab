a73f733f34fdfb44cc870a698dab1ade89b1ca88
nxdir-s <snadir177@gmail.com>
2025-10-18 01:04:36 -0400

    feat: start aws iac

--- iac/infrastructure.go
+++ iac/infrastructure.go
@@ -1,21 +1,30 @@
 package iac
 
 import (
+	"os"
+
 	"github.com/aws/aws-cdk-go/awscdk/v2"
+	ec2 "github.com/aws/aws-cdk-go/awscdk/v2/awsec2"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
+
+	"github.com/nxdir-s/homelab/iac/stacks"
 )
 
 type InfrastructureStackProps struct {
-	props awscdk.StackProps
+	awscdk.StackProps
 }
 
 func NewInfrastructureStack(scope constructs.Construct, id string, props *InfrastructureStackProps) awscdk.Stack {
 	var sprops awscdk.StackProps
 	if props != nil {
-		sprops = props.props
+		sprops = props.StackProps
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
+
+	ec2.Vpc_FromLookup(stack, jsii.String("vpc"), &ec2.VpcLookupOptions{
+		IsDefault: jsii.Bool(true),
+	})
 
 	return stack
 }
@@ -25,7 +34,42 @@ func main() {
 
 	app := awscdk.NewApp(nil)
 
-	NewInfrastructureStack(app, "Infrastructure", &InfrastructureStackProps{})
+	NewInfrastructureStack(app, "Infrastructure", &InfrastructureStackProps{
+		awscdk.StackProps{
+			Synthesizer: awscdk.NewDefaultStackSynthesizer(&awscdk.DefaultStackSynthesizerProps{}),
+			Env:         env(stacks.DevEast),
+		},
+	})
 
 	app.Synth(nil)
+}
+
+func env(name stacks.EnvRegion) *awscdk.Environment {
+	switch name {
+	case stacks.QAEast:
+		return &awscdk.Environment{
+			Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
+			Region:  jsii.String(stacks.AWSEast.String()),
+		}
+	case stacks.QAWest:
+		return &awscdk.Environment{
+			Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
+			Region:  jsii.String(stacks.AWSWest.String()),
+		}
+	case stacks.ProdEast:
+		return &awscdk.Environment{
+			Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
+			Region:  jsii.String(stacks.AWSEast.String()),
+		}
+	case stacks.ProdWest:
+		return &awscdk.Environment{
+			Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
+			Region:  jsii.String(stacks.AWSWest.String()),
+		}
+	default:
+		return &awscdk.Environment{
+			Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
+			Region:  jsii.String(stacks.AWSEast.String()),
+		}
+	}
 }
